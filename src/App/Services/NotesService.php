23a62937b127b23fd0808e7cd0293385ab34a263
Diego Muniz Cerqueira <cerqueiranews@gmail.com>
2016-12-06 21:43:37 -0200

    Acrescentada função para retornar ObjectID da string enviada como id. Incluído toArray ao getAll para retornar o array de documentos do cursor.

--- src/App/Services/NotesService.php
+++ src/App/Services/NotesService.php
@@ -1,19 +1,20 @@
 <?php
-
 namespace App\Services;
+
+use MongoDB\BSON\ObjectID;
 
 class NotesService extends BaseService
 {
 
     public function getOne($id)
     {
-        return $this->db->silex->notes->findOne(['_id' => $id]);
+        return $this->db->silex->notes->findOne(['_id' => new ObjectID($id)]);
         //return $this->db->fetchAssoc("SELECT * FROM notes WHERE id=?", [(int) $id]);
     }
 
     public function getAll()
     {
-        return $this->db->silex->notes->find();
+        return $this->db->silex->notes->find()->toArray();
         //return $this->db->fetchAll("SELECT * FROM notes");
     }
 
@@ -26,13 +27,13 @@ class NotesService extends BaseService
 
     function update($id, $note)
     {
-        return $this->db->silex->notes->updateOne(['_id' => $id], ['$set' => $note])->getModifiedCount();
+        return $this->db->silex->notes->updateOne(['_id' => new ObjectID($id)], ['$set' => $note])->getModifiedCount();
         //return $this->db->update('notes', $note, ['id' => $id]);
     }
 
     function delete($id)
     {
-        return $this->db->silex->notes->deleteOne(['_id' => $id])->getDeletedCount();
+        return $this->db->silex->notes->deleteOne(['_id' => new ObjectID($id)])->getDeletedCount();
         //return $this->db->delete("notes", array("id" => $id));
     }
 
